41afc01d75dd75717860aa6be5ae9525aaa2f400
Hakan Bilgic <hbilgic1992@gmail.com>
2022-10-16 14:32:12 +0200

    add image result component

--- Google_Clone/google-clone/pages/search.js
+++ Google_Clone/google-clone/pages/search.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import ImageResults from "../components/ImageResults";
 import SearchHeader from "../components/SearchHeader";
 import SearchResults from "../components/SearchResults";
 import Response from "../Response";
@@ -15,15 +16,19 @@ export default function Search({ results }) {
       {/* Search Header */}
       <SearchHeader />
 
-      {/* Search Results */}
-      <SearchResults results={results} />
+      {/* Search Web and Images Results */}
+      {router.query.searchType === "image" ? (
+        <ImageResults results={results} />
+      ) : (
+        <SearchResults results={results} />
+      )}
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
   const startIndex = context.query.start || "1";
-  const mockData = false;
+  const mockData = true;
   const data = mockData
     ? Response
     : await fetch(
