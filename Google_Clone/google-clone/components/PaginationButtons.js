9021c9bf4e231411f62aae1b2d52882c8f3aba0c
Hakan Bilgic <hbilgic1992@gmail.com>
2022-10-16 13:48:03 +0200

    create pagination component

---
+++ Google_Clone/google-clone/components/PaginationButtons.js
@@ -0,0 +1,38 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const PaginationButtons = () => {
+  const router = useRouter();
+  const startIndex = Number(router.query.start);
+  console.log(startIndex);
+  return (
+    <div className="text-blue-700 flex px-9 pb-4 justify-between sm:justify-start sm:space-x-44 sm::px-0">
+      {startIndex < 90 && (
+        <Link
+          href={`/search?term=${router.query.term}&searchType=${
+            router.query.searchType
+          }&start=${startIndex + 10}`}
+        >
+          <div className="cursor-pointer flex flex-col items-center">
+            <ChevronRightIcon className="h-5" />
+            <p>Next</p>
+          </div>
+        </Link>
+      )}
+      {startIndex > 10 && (
+        <Link
+          href={`/search?term=${router.query.term}&searchType=${
+            router.query.searchType
+          }&start=${startIndex - 10}`}
+        >
+          <div className="cursor-pointer flex flex-col items-center">
+            <ChevronLeftIcon className="h-5" />
+            <p>Previous</p>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
+};
+export default PaginationButtons;
