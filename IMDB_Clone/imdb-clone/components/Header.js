116b5af808c2d0636b58f54ab2eb1b49a47a913e
Hakan Bilgic <hbilgic1992@gmail.com>
2022-10-17 12:01:11 +0200

    create header component

--- IMDB_Clone/imdb-clone/components/Header.js
+++ IMDB_Clone/imdb-clone/components/Header.js
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
+import {
+  HomeIcon,
+  UserIcon,
+  PhoneIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/solid";
+
 const Header = () => {
   return (
     <div>
-      <p>Header</p>
+      <div className="">
+        <HeaderIcon Icon={HomeIcon} title="HOME" />
+        <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
+        <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
+        <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+      </div>
+      <Image
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/640px-IMDB_Logo_2016.svg.png"
+        width={100}
+        height={100}
+        alt="IMDB Logo"
+      />
     </div>
   );
 };
---
+++ IMDB_Clone/imdb-clone/components/HeaderIcon.js
@@ -0,0 +1,9 @@
+const HeaderIcon = ({ Icon, title }) => {
+  return (
+    <div>
+      <Icon className="h-8" />
+      <p>{title}</p>
+    </div>
+  );
+};
+export default HeaderIcon;
