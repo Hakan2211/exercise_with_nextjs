eb0af746a3518291361722cf10410474866f7486
Hakan Bilgic <hbilgic1992@gmail.com>
2022-10-19 12:26:54 +0200

    create navbar

--- IMDB_Clone/imdb-clone/components/Navbar.js
+++ IMDB_Clone/imdb-clone/components/Navbar.js
@@ -1,7 +1,19 @@
+import { useRouter } from "next/router";
+import request from "../utils/request.js";
+
 const Navbar = () => {
+  const router = useRouter();
   return (
-    <div>
-      <p> Navbar</p>
+    <div className="flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
+      {Object.entries(request).map(([key, { title, url }]) => (
+        <h2
+          onClick={() => router.push(`?genre=${key}`)}
+          className="m-6 cursor-pointer hover:text-white active:text-red-400"
+          key={key}
+        >
+          {title}
+        </h2>
+      ))}
     </div>
   );
 };
