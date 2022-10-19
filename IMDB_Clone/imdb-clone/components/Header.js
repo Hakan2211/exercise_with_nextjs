e33236ef460ffd1ac54de9333671b42d3d7a3351
Hakan Bilgic <hbilgic1992@gmail.com>
2022-10-19 11:55:49 +0200

    style the header section

--- IMDB_Clone/imdb-clone/components/Header.js
+++ IMDB_Clone/imdb-clone/components/Header.js
@@ -9,8 +9,8 @@ import {
 
 const Header = () => {
   return (
-    <div>
-      <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
@@ -19,8 +19,9 @@ const Header = () => {
       <Image
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/640px-IMDB_Logo_2016.svg.png"
         width={100}
-        height={100}
+        height={60}
         alt="IMDB Logo"
+        className="cursor-pointer active:brightness-110"
       />
     </div>
   );
