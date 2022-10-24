4b69dbd7a4ae6bbe7fb114d79d12b82551b5b0ec
Hakan Bilgic <hbilgic1992@gmail.com>
2022-10-24 12:27:22 +0200

    implement sticky header

--- Instagram_Clone/instagram-clone/components/Header.js
+++ Instagram_Clone/instagram-clone/components/Header.js
@@ -4,44 +4,46 @@ import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
-          layout="fill"
-          className="object-contain"
-          alt="logo"
-        />
-      </div>
-      <div className="h-24 w-10 relative lg:hidden cursor-pointer">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png"
-          layout="fill"
-          className="object-contain"
-          alt="logo"
-        />
-      </div>
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <SearchIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
+            layout="fill"
+            className="object-contain"
+            alt="logo"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
-      {/* Right */}
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="https://image.gala.de/20078934/t/08/v16/w1440/r1/-/steve-jobs--4830684-.jpg"
-          alt="user image"
-          className="h-10 rounded-full cursor-pointer"
-        />
+        <div className="h-24 w-10 relative lg:hidden cursor-pointer">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png"
+            layout="fill"
+            className="object-contain"
+            alt="logo"
+          />
+        </div>
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://image.gala.de/20078934/t/08/v16/w1440/r1/-/steve-jobs--4830684-.jpg"
+            alt="user image"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
